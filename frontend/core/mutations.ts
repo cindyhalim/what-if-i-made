5ec3666d9a96edff2b970c8043f4a3b6101149c1
Cindy Halim <jcindyhalim@gmail.com>
2022-06-18 15:57:18 -0400

    feat: update service url

--- frontend/core/mutations.ts
+++ frontend/core/mutations.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { formatCurrencyToInt } from "./money"
 
-const baseUrl = "http://127.0.0.1:8000/income"
+const baseUrl = `${process.env.REACT_APP_SERVICE_URL}/income`
 
 const regionToCodeMap: { [key: string]: string } = {
   Alberta: "AB",
